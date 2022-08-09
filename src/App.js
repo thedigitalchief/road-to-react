7ac3b08a5fecf522db33436116bb963d549f9322
thedigitalchief <me@dylanhnguyen.com>
2022-08-08 20:16:59 -0700

    exploring lists and arrays

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+
+  ]
+  const title = "Road to React";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <h1> Hello {title} World </h1>
     </div>
   );
 }

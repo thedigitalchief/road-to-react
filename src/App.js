185b5eb582199fc3516884db730a327935166157
thedigitalchief <me@dylanhnguyen.com>
2022-08-08 20:39:59 -0700

    added Item component, child of List

--- src/App.js
+++ src/App.js
@@ -68,4 +68,16 @@ const List = (props) => (
   </ul>
 );
 
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+</li> );
+
+
+
 export default App;
